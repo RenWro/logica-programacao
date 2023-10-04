af79c53637f786e4f290a5a331167d1985e11c87
Ren Wro <134458911+RenWro@users.noreply.github.com>
2023-10-04 16:34:23 -0300

    Aula 04 de outubro de 2023

---
+++ script.js
@@ -0,0 +1,34 @@
+//console.log ('Olá Console!');
+
+//document.write ('Olá Documento!');
+
+//window.alert('Olá, Janela!');
+
+
+//Lógica de programação 041023
+//Problema: obter 3 notas de um aluno, calcular média, exibir média e exibir a situação do aluno aprovado, em exame ou reprovado,
+
+console.log('Inicio do programa');
+
+
+//declaração de variáveis
+//const para variáveis que não variam e let para variáveis que  variam (var é sem escopo global) - var (escopo global) - escopo fora do bloco. let (escopo local) - escopo restrito ao bloco. const (não permitindo reatribuição e nem redeclaração) - constante
+
+const nota1 = parseInt(prompt('Digite a nota 1:'));
+const nota2 = parseInt(prompt('Digite a nota 2:'));
+const nota3 = parseInt(prompt('Digite a nota 3:'));
+
+
+
+
+//cálculo da média
+const media = (nota1 + nota2 + nota3) / 3;
+
+console.log('A nota 1 é: ', nota1);
+console.log('A nota 2 é: ', nota2);
+console.log('A nota 3 é: ', nota3);
+
+console.log ('A média da estudante é: ', media);
+
+console.log ('Fim do programa');
+
